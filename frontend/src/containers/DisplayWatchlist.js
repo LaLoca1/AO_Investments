3d4fbe170b6b0c01b8edc1f53b995b6002c4ea5c
Alex Olalla <alex.olalla13@gmail.com>
2024-01-03 17:03:52 +0000

    edit function works

--- frontend/src/containers/DisplayWatchlist.js
+++ frontend/src/containers/DisplayWatchlist.js
@@ -1,11 +1,50 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
-// import EditWatchlistItem from "./EditWatchlistItem"; // Import your new component
+import EditWatchlistItem from "./EditWatchlistItem"; // Import your new component
 
 const DisplayWatchlist = () => {
   const [watchlistItems, setWatchlistItems] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [editingItem, setEditingItem] = useState(null);
+
+  const handleEdit = (item) => {
+    setEditingItem(item);
+  };
+
+  const handleSaveEdit = async (editedItem) => {
+    const config = {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-CSRFToken": Cookies.get("csrftoken"),
+      },
+    };
+
+    try {
+      const response = await axios.put(
+        `/watchlist/api/edit-watchlist-item/${editedItem.id}`,
+        editedItem,
+        config
+      );
+
+      if (response.status === 200) {
+        setWatchlistItems((prevItems) =>
+          prevItems.map((item) =>
+            item.id === editedItem.id ? editedItem : item
+          )
+        );
+
+        setEditingItem(null);
+      }
+    } catch (error) {
+      console.error("Error updating watchlist item:", error);
+    }
+  };
+
+  const handleCancelEdit = () => {
+    setEditingItem(null);
+  };
 
   useEffect(() => {
     fetchData();
@@ -25,7 +64,9 @@ const DisplayWatchlist = () => {
 
   const handleDelete = async (id) => {
     // Add a confirmation dialog before deletion
-    const confirmDelete = window.confirm("Are you sure you want to delete this item?");
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this item?"
+    );
     if (!confirmDelete) {
       return;
     }
@@ -58,13 +99,20 @@ const DisplayWatchlist = () => {
             <li key={item.id}>
               {item.id} - {item.ticker} - {item.quantity} - {item.price} -{" "}
               {item.sector} - {new Date(item.trade_date).toLocaleDateString()} -{" "}
-              {item.comments} - {item.transactionType} 
+              {item.comments} - {item.transactionType}
+              <button onClick={() => handleEdit(item)}>Edit</button>
               <button onClick={() => handleDelete(item.id)}>Delete</button>
             </li>
           ))}
+          {editingItem && (
+            <EditWatchlistItem 
+            item={editingItem} 
+            onSave={handleSaveEdit} 
+            onCancel={handleCancelEdit}
+            /> 
+          )} 
         </ul>
       )}
-
     </div>
   );
 };
--- frontend/src/containers/EditWatchlistItem.js
+++ frontend/src/containers/EditWatchlistItem.js
@@ -3,130 +3,83 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import CSRFToken from "../components/CSRFToken";
 
-const EditWatchlistItem = ({ item, onClose }) => {
-  const [editedItem, setEditedItem] = useState({
-    ticker: item.ticker,
-    quantity: item.quantity,
-    price: item.price,
-    sector: item.sector,
-    trade_date: item.trade_date.split("T")[0],
-    comments: item.comments,
-  });
-
-  const [error, setError] = useState(null);
+const EditWatchlistItem = ({ item, onSave, onCancel }) => {
+  const [editedItem, setEditedItem] = useState({ ...item });
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setEditedItem({ ...editedItem, [name]: value });
+    setEditedItem({ ...editedItem, [e.target.name]: e.target.value });
   };
 
-  const handleEdit = async () => {
-    const config = {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "X-CSRFToken": Cookies.get("csrftoken"),
-      },
-    };
-
-    try {
-      await axios.put(
-        `/watchlist/api/edit-watchlist-item/${item.id}`,
-        editedItem,
-        config
-      );
-      onClose(); // Close the modal after successful edit
-    } catch (error) {
-      console.error("Error editing watchlist item:", error);
-      setError("An error occurred while editing the item.");
-    }
-  };
-
-  const handleCancel = () => {
-    const confirmCancel = window.confirm(
-      "Are you sure you want to cancel? Any unsaved changes will be lost."
-    );
-    if (confirmCancel) {
-      onClose();
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSave(editedItem);
   };
 
   return (
-    <div className="edit-watchlist-item-container">
-      <h2>Edit Watchlist Item</h2>
-      <form onSubmit={handleEdit}>
-        <CSRFToken />
-        <label className="form-label">Ticker:</label>
-        <input
-          type="text"
-          name="ticker"
-          value={editedItem.ticker}
-          onChange={handleChange}
-          className="form-input"
-          required
-        />
+    <div className="edit-popup">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Ticker:
+          <input
+            type="text"
+            name="ticker"
+            value={editedItem.ticker}
+            onChange={handleChange}
+          />
+        </label>
 
-        <label className="form-label">Quantity:</label>
-        <input
-          type="number"
-          name="quantity"
-          value={editedItem.quantity}
-          onChange={handleChange}
-          className="form-input"
-          required
-        />
+        <label>
+          Quantity:
+          <input
+            type="number"
+            name="quantity"
+            value={editedItem.quantity}
+            onChange={handleChange}
+          />
+        </label>
 
-        <label className="form-label">Price:</label>
-        <input
-          type="number"
-          name="price"
-          value={editedItem.price}
-          onChange={handleChange}
-          className="form-input"
-          required
-        />
+        <label>
+          Price:
+          <input
+            type="number"
+            name="price"
+            value={editedItem.price}
+            onChange={handleChange}
+          />
+        </label>
 
-        <label className="form-label">Sector:</label>
-        <input
-          type="text"
-          name="sector"
-          value={editedItem.sector}
-          onChange={handleChange}
-          className="form-input"
-          required
-        />
+        <label>
+          Sector:
+          <input
+            type="text"
+            name="sector"
+            value={editedItem.sector}
+            onChange={handleChange}
+          />
+        </label>
 
-        <label className="form-label">Trade Date:</label>
-        <input
-          type="date"
-          name="trade_date"
-          value={editedItem.trade_date}
-          onChange={handleChange}
-          className="form-input"
-          required
-        />
+        <label>
+          Trade Date:
+          <input
+            type="date"
+            name="trade_date"
+            value={new Date(editedItem.trade_date).toISOString().split("T")[0]}
+            onChange={handleChange}
+          />
+        </label>
 
-        <label className="form-label">Comments:</label>
-        <input
-          type="text"
-          name="comments"
-          value={editedItem.comments}
-          onChange={handleChange}
-          className="form-input"
-          required
-        />
-
-        <div className="form-buttons">
-          <button type="button" onClick={handleEdit}>
-            Save Changes
-          </button>
-          <button type="button" onClick={handleCancel}>
-            Cancel
-          </button>
-        </div>
+        <label>
+          Comments:
+          <textarea
+            name="comments"
+            value={editedItem.comments}
+            onChange={handleChange}
+          />
+        </label>
+        
+        <button type="submit">Save</button>
+        <button onClick={onCancel}>Cancel</button>
       </form>
-
-      {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
   );
 };
