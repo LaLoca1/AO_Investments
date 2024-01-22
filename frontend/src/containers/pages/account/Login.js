979705c5b66f6686470505b7e3884b9ef3d147cb
Alex Olalla <alex.olalla13@gmail.com>
2024-01-22 22:21:59 +0000

    adjusted pages

--- frontend/src/containers/pages/account/Login.js
+++ frontend/src/containers/pages/account/Login.js
@@ -1,70 +1,75 @@
-import React, { useState } from 'react';
-import { Navigate, Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { login } from '../../../actions/auth';
-import CSRFToken from '../../../components/CSRFToken';
+import React, { useState } from "react";
+import { Navigate, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { login } from "../../../actions/auth";
+import CSRFToken from "../../../components/CSRFToken";
+import './account.css' 
 
 const Login = ({ login, isAuthenticated }) => {
-    const [formData, setFormData] = useState({
-        username: '',
-        password: ''
-    });
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
 
-    const { username, password } = formData;
+  const { username, password } = formData;
 
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const onSubmit = e => {
-        e.preventDefault();
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-        login(username, password);
-    };
+    login(username, password);
+  };
 
-    if (isAuthenticated)
-        return <Navigate to='/dashboard'/>;
+  if (isAuthenticated) return <Navigate to="/dashboard" />;
 
-    return (
-        <div className='container mt-5'>
-            <h1>Sign In</h1>
-            <p>Sign into your Session Auth account</p>
-            <form onSubmit={e => onSubmit(e)}>
-                <CSRFToken />
-                <div className='form-group'>
-                    <label className='form-label'>Username: </label>
-                    <input
-                        className='form-control'
-                        type='text'
-                        placeholder='Username*'
-                        name='username'
-                        onChange={e => onChange(e)}
-                        value={username}
-                        required
-                    />
-                </div>
-                <div className='form-group'>
-                    <label className='form-label mt-3'>Password: </label>
-                    <input
-                        className='form-control'
-                        type='password'
-                        placeholder='Password*'
-                        name='password'
-                        onChange={e => onChange(e)}
-                        value={password}
-                        minLength='6'
-                        required
-                    />
-                </div>
-                <button className='btn btn-primary mt-3' type='submit'>Login</button>
-            </form>
-            <p className='mt-3'>
-                Don't have an Account? <Link to='/register'>Sign Up</Link>
-            </p>
-        </div>
-    );
+  return (
+    <div className="main-content">
+      <div className="container mt-5">
+        <h1>Sign In</h1>
+        <p>Sign into your Session Auth account</p>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <CSRFToken />
+          <div className="form-group">
+            <label className="form-label">Username: </label>
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Username*"
+              name="username"
+              onChange={(e) => onChange(e)}
+              value={username}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label mt-3">Password: </label>
+            <input
+              className="form-control"
+              type="password"
+              placeholder="Password*"
+              name="password"
+              onChange={(e) => onChange(e)}
+              value={password}
+              minLength="6"
+              required
+            />
+          </div>
+          <button className="btn btn-primary mt-3" type="submit">
+            Login
+          </button>
+        </form>
+        <p className="mt-3">
+          Don't have an Account? <Link to="/register">Sign Up</Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
-const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
 });
 
 export default connect(mapStateToProps, { login })(Login);
--- frontend/src/containers/pages/account/UserProfile.js
+++ frontend/src/containers/pages/account/UserProfile.js
@@ -1,100 +1,108 @@
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
-import { update_profile } from '../../../actions/profile';
-import { delete_account } from '../../../actions/auth';
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { update_profile } from "../../../actions/profile";
+import { delete_account } from "../../../actions/auth";
+import './account.css'
 
 const UserProfile = ({
-    delete_account,
-    update_profile,
-    first_name_global,
-    last_name_global,
-    email_global
+  delete_account,
+  update_profile,
+  first_name_global,
+  last_name_global,
+  email_global,
 }) => {
-    const [formData, setFormData] = useState({
-        first_name: '',
-        last_name: '',
-        email: ''
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+  });
+
+  const { first_name, last_name, email } = formData;
+
+  useEffect(() => {
+    setFormData({
+      first_name: first_name_global,
+      last_name: last_name_global,
+      email: email_global,
     });
+  }, [first_name_global]);
 
-    const { first_name, last_name, email } = formData;
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    useEffect(() => {
-        setFormData({
-            first_name: first_name_global,
-            last_name: last_name_global,
-            email: email_global
-        });
-    }, [first_name_global]);
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    update_profile(first_name, last_name, email);
+  };
 
-    const onSubmit = e => {
-        e.preventDefault();
-
-        update_profile(first_name, last_name, email); 
-    };
-
-    return (
-        <div className='container'>
-            <h1 className='mt-3'>User Profile Page</h1>
-            <p className='mt-3 mb-3'>Update your user profile below:</p>
-            <form onSubmit={e => onSubmit(e)}>
-                <div className='form-group'>
-                    <label className='form-label' htmlFor='first_name'>First Name</label>
-                    <input
-                        className='form-control'
-                        type='text'
-                        name='first_name'
-                        placeholder={`${first_name_global}`}
-                        onChange={e => onChange(e)}
-                        value={first_name}
-                    />
-                </div>
-                <div className='form-group'>
-                    <label className='form-label mt-3' htmlFor='last_name'>Last Name</label>
-                    <input
-                        className='form-control'
-                        type='text'
-                        name='last_name'
-                        placeholder={`${last_name_global}`}
-                        onChange={e => onChange(e)}
-                        value={last_name}
-                    />
-                </div>
-                <div className='form-group'>
-                    <label className='form-label mt-3' htmlFor='email'>Email</label>
-                    <input
-                        className='form-control'
-                        type='text'
-                        name='email'
-                        placeholder={`${email_global}`}
-                        onChange={e => onChange(e)}
-                        value={email}
-                    />
-                </div>
-                <button className='btn btn-primary mt-3' type='submit'>Update Profile</button>
-            </form>
-            <p className='mt-5'>
-                Click the button below to delete your user account:
-            </p>
-            <a
-                className='btn btn-danger'
-                href='#!'
-                onClick={delete_account}
-            >
-                Delete Account
-            </a>
-        </div>
-    )
+  return (
+    <div className="container">
+      <div className="main-content">
+        <h1 className="mt-3">User Profile Page</h1>
+        <p className="mt-3 mb-3">Update your user profile below:</p>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <div className="form-group">
+            <label className="form-label" htmlFor="first_name">
+              First Name
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              name="first_name"
+              placeholder={`${first_name_global}`}
+              onChange={(e) => onChange(e)}
+              value={first_name}
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label mt-3" htmlFor="last_name">
+              Last Name
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              name="last_name"
+              placeholder={`${last_name_global}`}
+              onChange={(e) => onChange(e)}
+              value={last_name}
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label mt-3" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              name="email"
+              placeholder={`${email_global}`}
+              onChange={(e) => onChange(e)}
+              value={email}
+            />
+          </div>
+          <button className="btn btn-primary mt-3" type="submit">
+            Update Profile
+          </button>
+        </form>
+        <p className="mt-5">
+          Click the button below to delete your user account:
+        </p>
+        <a className="btn btn-danger" href="#!" onClick={delete_account}>
+          Delete Account
+        </a>
+      </div>
+    </div>
+  );
 };
 
-const mapStateToProps = state => ({
-    first_name_global: state.profile.first_name,
-    last_name_global: state.profile.last_name,
-    email_global: state.profile.email,
+const mapStateToProps = (state) => ({
+  first_name_global: state.profile.first_name,
+  last_name_global: state.profile.last_name,
+  email_global: state.profile.email,
 });
 
-export default connect(mapStateToProps, { 
-    delete_account,
-    update_profile
- })(UserProfile);
+export default connect(mapStateToProps, {
+  delete_account,
+  update_profile,
+})(UserProfile);
